4ed49de582baa9d708f760f5f78b61473cd9ecd1
Maxim Andreev <stat1c@ngs.ru>
2021-01-19 14:10:07 +0700

    in progress

--- src/components/Item.js
+++ src/components/Item.js
@@ -17,7 +17,10 @@ class Item extends React.Component{
     this.setState(prevState => ({
       isShow: !prevState
     }));
-    console.log(this.state)
+  }
+
+  showPopup(){
+    alert(this.props.user.name + ' Добавлен в корзину!')
   }
  
 
@@ -27,7 +30,7 @@ class Item extends React.Component{
 
     if (isShow === true){
       return (
-        <div className="item-card">
+        <div className="item-card" onClick={() => this.showPopup()}>
           <p className="item-game">{game}</p>
           <img className="item-img" src = {image} />
           <p className="item-card__header">{name}</p>  
@@ -37,7 +40,7 @@ class Item extends React.Component{
             <p className="oldprice">{oldprice}</p>  
           </div> 
           {counter > 1000 && <p className="counter">Популярно! купили более {counter} раз!</p>}
-          {additional.map((add, index) => <Additional key={index} name={add.name} description={add.description} price={add.price} oldprice={add.oldprice} />)}
+          {additional.slice(0,1).map((add, index) => <Additional key={index} name={add.name} description={add.description} price={add.price} oldprice={add.oldprice} />)}
           {additional.length > 2 && <button onClick={() => this.showMore()}>Показать больше</button>}
           <button onClick={() => this.deleteItem()}>Удалить</button>
         </div>
